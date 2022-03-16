90b5aee6efffb5f12ae2a28812fddcfa6f4a5a95
Ramirisu <labyrinth.ramirisu@gmail.com>
2022-03-16 13:16:45 +0800

    Use getpwuid() instead if getlogin_r() fails

--- source/system.unix.cpp
+++ source/system.unix.cpp
@@ -1,6 +1,8 @@
 #include <iris/system.hpp>
 
 #include <limits.h>
+#include <pwd.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 namespace iris {
@@ -20,6 +22,10 @@ std::optional<std::string> get_user_name() noexcept
     char username[LOGIN_NAME_MAX];
     if (getlogin_r(username, LOGIN_NAME_MAX) == 0) {
         return username;
+    }
+
+    if (auto pwd = getpwuid(getuid()); pwd != nullptr) {
+        return pwd->pw_name;
     }
 
     return std::nullopt;
