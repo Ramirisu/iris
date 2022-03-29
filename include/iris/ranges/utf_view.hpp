11960f1240472e8bd3ae94fdb2096083a693b220
Ramirisu <labyrinth.ramirisu@gmail.com>
2022-03-30 01:24:00 +0800

    Swap code fragments.

--- include/iris/ranges/utf_view.hpp
+++ include/iris/ranges/utf_view.hpp
@@ -13,190 +13,6 @@ template <std::ranges::input_range Range, typename Unicode, typename UTF>
     // clang-format off
     requires std::ranges::view<Range>
 // clang-format on
-class from_utf_view
-    : public std::ranges::view_interface<from_utf_view<Range, Unicode, UTF>> {
-public:
-    class iterator {
-        using encoder = utf<Unicode, UTF>;
-
-    public:
-        using iterator_concept
-            = std::conditional_t<std::ranges::forward_range<Range>,
-                                 std::forward_iterator_tag,
-                                 std::input_iterator_tag>;
-        using iterator_category = std::conditional_t<
-            std::derived_from<
-                typename std::iterator_traits<
-                    std::ranges::iterator_t<Range>>::iterator_category,
-                std::forward_iterator_tag>,
-            std::forward_iterator_tag,
-            std::input_iterator_tag>;
-        using value_type = typename encoder::unicode_result_type;
-        using difference_type = std::ptrdiff_t;
-
-        iterator() requires
-            std::default_initializable<std::ranges::iterator_t<Range>>
-        = default;
-
-        constexpr iterator(Range& parent, std::ranges::iterator_t<Range> curr)
-            : parent_(&parent)
-            , curr_(std::move(curr))
-        {
-            next();
-        }
-
-        iterator(const iterator&) = default;
-        iterator& operator=(const iterator&) = default;
-
-        iterator(iterator&&) = default;
-        iterator& operator=(iterator&&) = default;
-
-        constexpr const value_type& operator*() const noexcept
-        {
-            return value_;
-        }
-
-        constexpr iterator& operator++()
-        {
-            next();
-            return *this;
-        }
-
-        constexpr iterator operator++(int)
-        {
-            auto tmp = *this;
-            ++*this;
-            return tmp;
-        }
-
-        friend constexpr bool operator==(const iterator& lhs,
-                                         const iterator& rhs)
-        {
-            return lhs.curr_ == rhs.curr_;
-        }
-
-    private:
-        void next()
-        {
-            value_ = encoder::decode(curr_, std::ranges::end(*parent_));
-        }
-
-        Range* parent_ {};
-        std::ranges::iterator_t<Range> curr_ {};
-        value_type value_;
-    };
-
-    class sentinel {
-    public:
-        sentinel() = default;
-
-        constexpr sentinel(std::ranges::sentinel_t<Range> last)
-            : last_(std::move(last))
-        {
-        }
-
-        friend constexpr bool operator==(const iterator& lhs,
-                                         const sentinel& rhs)
-        {
-            return !*lhs && (*lhs).error() == utf_error::eof;
-        }
-
-    private:
-        std::ranges::sentinel_t<Range> last_;
-    };
-
-    // clang-format off
-    from_utf_view() requires std::default_initializable<Range> = default;
-    // clang-format on
-
-    constexpr explicit from_utf_view(Range range) noexcept(
-        std::is_nothrow_move_constructible_v<Range>)
-        : range_(std::move(range))
-    {
-    }
-
-    // clang-format off
-    [[nodiscard]] constexpr Range base() const& 
-        noexcept(std::is_nothrow_copy_constructible_v<Range>) 
-        requires std::copy_constructible<Range>
-    // clang-format on
-    {
-        return range_;
-    }
-
-    // clang-format off
-    [[nodiscard]] constexpr Range base() && 
-        noexcept(std::is_nothrow_move_constructible_v<Range>) 
-        requires std::move_constructible<Range>
-    // clang-format on
-    {
-        return std::move(range_);
-    }
-
-    [[nodiscard]] constexpr iterator begin()
-    {
-        return { range_, std::ranges::begin(range_) };
-    }
-
-    [[nodiscard]] constexpr sentinel end()
-    {
-        return { std::ranges::end(range_) };
-    }
-
-    // clang-format off
-    [[nodiscard]] constexpr auto size()  
-        noexcept(noexcept(std::ranges::size(range_))) 
-        requires (sizeof(UTF) == 4 && std::ranges::sized_range<Range>)
-    // clang-format on
-    {
-        return std::ranges::size(range_);
-    }
-
-    // clang-format off
-private:
-    // clang-format on
-    Range range_ {};
-};
-
-template <typename Range>
-from_utf_view(Range&&) -> from_utf_view<std::ranges::views::all_t<Range>,
-                                        std::uint32_t,
-                                        std::ranges::range_value_t<Range>>;
-
-namespace views {
-#if defined(__GNUC__) && __GNUC__ == 10
-    inline constexpr std::ranges::views::__adaptor::_RangeAdaptorClosure
-        from_utf
-        = []<std::ranges::viewable_range Range>(Range&& range) {
-              return from_utf_view { std::forward<Range>(range) };
-          };
-#else
-    struct __from_utf_view_fn
-#if defined(_MSC_VER)
-        : std::ranges::_Pipe::_Base<__from_utf_view_fn>
-#elif defined(__GNUC__) && __GNUC__ >= 11
-        : std::ranges::views::__adaptor::_RangeAdaptorClosure
-#endif
-    {
-        template <std::ranges::viewable_range Range>
-        [[nodiscard]] constexpr auto operator()(Range&& range) const
-            // clang-format off
-            noexcept(noexcept(from_utf_view { std::forward<Range>(range) }))
-            requires requires { from_utf_view { std::forward<Range>(range) }; }
-        // clang-format on
-        {
-            return from_utf_view { std::forward<Range>(range) };
-        }
-    };
-
-    inline constexpr __from_utf_view_fn from_utf {};
-#endif
-}
-
-template <std::ranges::input_range Range, typename Unicode, typename UTF>
-    // clang-format off
-    requires std::ranges::view<Range>
-// clang-format on
 class to_utf_view
     : public std::ranges::view_interface<to_utf_view<Range, Unicode, UTF>> {
 public:
@@ -467,6 +283,190 @@ namespace views {
     };
 
     inline constexpr __to_utf32_view_fn to_utf32 {};
+#endif
+}
+
+template <std::ranges::input_range Range, typename Unicode, typename UTF>
+    // clang-format off
+    requires std::ranges::view<Range>
+// clang-format on
+class from_utf_view
+    : public std::ranges::view_interface<from_utf_view<Range, Unicode, UTF>> {
+public:
+    class iterator {
+        using encoder = utf<Unicode, UTF>;
+
+    public:
+        using iterator_concept
+            = std::conditional_t<std::ranges::forward_range<Range>,
+                                 std::forward_iterator_tag,
+                                 std::input_iterator_tag>;
+        using iterator_category = std::conditional_t<
+            std::derived_from<
+                typename std::iterator_traits<
+                    std::ranges::iterator_t<Range>>::iterator_category,
+                std::forward_iterator_tag>,
+            std::forward_iterator_tag,
+            std::input_iterator_tag>;
+        using value_type = typename encoder::unicode_result_type;
+        using difference_type = std::ptrdiff_t;
+
+        iterator() requires
+            std::default_initializable<std::ranges::iterator_t<Range>>
+        = default;
+
+        constexpr iterator(Range& parent, std::ranges::iterator_t<Range> curr)
+            : parent_(&parent)
+            , curr_(std::move(curr))
+        {
+            next();
+        }
+
+        iterator(const iterator&) = default;
+        iterator& operator=(const iterator&) = default;
+
+        iterator(iterator&&) = default;
+        iterator& operator=(iterator&&) = default;
+
+        constexpr const value_type& operator*() const noexcept
+        {
+            return value_;
+        }
+
+        constexpr iterator& operator++()
+        {
+            next();
+            return *this;
+        }
+
+        constexpr iterator operator++(int)
+        {
+            auto tmp = *this;
+            ++*this;
+            return tmp;
+        }
+
+        friend constexpr bool operator==(const iterator& lhs,
+                                         const iterator& rhs)
+        {
+            return lhs.curr_ == rhs.curr_;
+        }
+
+    private:
+        void next()
+        {
+            value_ = encoder::decode(curr_, std::ranges::end(*parent_));
+        }
+
+        Range* parent_ {};
+        std::ranges::iterator_t<Range> curr_ {};
+        value_type value_;
+    };
+
+    class sentinel {
+    public:
+        sentinel() = default;
+
+        constexpr sentinel(std::ranges::sentinel_t<Range> last)
+            : last_(std::move(last))
+        {
+        }
+
+        friend constexpr bool operator==(const iterator& lhs,
+                                         const sentinel& rhs)
+        {
+            return !*lhs && (*lhs).error() == utf_error::eof;
+        }
+
+    private:
+        std::ranges::sentinel_t<Range> last_;
+    };
+
+    // clang-format off
+    from_utf_view() requires std::default_initializable<Range> = default;
+    // clang-format on
+
+    constexpr explicit from_utf_view(Range range) noexcept(
+        std::is_nothrow_move_constructible_v<Range>)
+        : range_(std::move(range))
+    {
+    }
+
+    // clang-format off
+    [[nodiscard]] constexpr Range base() const& 
+        noexcept(std::is_nothrow_copy_constructible_v<Range>) 
+        requires std::copy_constructible<Range>
+    // clang-format on
+    {
+        return range_;
+    }
+
+    // clang-format off
+    [[nodiscard]] constexpr Range base() && 
+        noexcept(std::is_nothrow_move_constructible_v<Range>) 
+        requires std::move_constructible<Range>
+    // clang-format on
+    {
+        return std::move(range_);
+    }
+
+    [[nodiscard]] constexpr iterator begin()
+    {
+        return { range_, std::ranges::begin(range_) };
+    }
+
+    [[nodiscard]] constexpr sentinel end()
+    {
+        return { std::ranges::end(range_) };
+    }
+
+    // clang-format off
+    [[nodiscard]] constexpr auto size()  
+        noexcept(noexcept(std::ranges::size(range_))) 
+        requires (sizeof(UTF) == 4 && std::ranges::sized_range<Range>)
+    // clang-format on
+    {
+        return std::ranges::size(range_);
+    }
+
+    // clang-format off
+private:
+    // clang-format on
+    Range range_ {};
+};
+
+template <typename Range>
+from_utf_view(Range&&) -> from_utf_view<std::ranges::views::all_t<Range>,
+                                        std::uint32_t,
+                                        std::ranges::range_value_t<Range>>;
+
+namespace views {
+#if defined(__GNUC__) && __GNUC__ == 10
+    inline constexpr std::ranges::views::__adaptor::_RangeAdaptorClosure
+        from_utf
+        = []<std::ranges::viewable_range Range>(Range&& range) {
+              return from_utf_view { std::forward<Range>(range) };
+          };
+#else
+    struct __from_utf_view_fn
+#if defined(_MSC_VER)
+        : std::ranges::_Pipe::_Base<__from_utf_view_fn>
+#elif defined(__GNUC__) && __GNUC__ >= 11
+        : std::ranges::views::__adaptor::_RangeAdaptorClosure
+#endif
+    {
+        template <std::ranges::viewable_range Range>
+        [[nodiscard]] constexpr auto operator()(Range&& range) const
+            // clang-format off
+            noexcept(noexcept(from_utf_view { std::forward<Range>(range) }))
+            requires requires { from_utf_view { std::forward<Range>(range) }; }
+        // clang-format on
+        {
+            return from_utf_view { std::forward<Range>(range) };
+        }
+    };
+
+    inline constexpr __from_utf_view_fn from_utf {};
 #endif
 }
 
