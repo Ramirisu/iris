6b215a994998ae4f8cc87f9d845f0bc56f54d562
Ramirisu <labyrinth.ramirisu@gmail.com>
2022-04-22 01:35:51 +0800

    Add mising includes for ranges.hpp.

--- include/iris/ranges.hpp
+++ include/iris/ranges.hpp
@@ -4,11 +4,13 @@
 
 #include <iris/ranges/range_adaptor_closure.hpp>
 
+#include <iris/ranges/adjacent_transform_view.hpp>
 #include <iris/ranges/adjacent_view.hpp>
 #include <iris/ranges/base64_view.hpp>
 #include <iris/ranges/chunk_by_view.hpp>
 #include <iris/ranges/chunk_view.hpp>
 #include <iris/ranges/elements_of.hpp>
+#include <iris/ranges/enumerate_view.hpp>
 #include <iris/ranges/join_with_view.hpp>
 #include <iris/ranges/slide_view.hpp>
 #include <iris/ranges/to.hpp>
