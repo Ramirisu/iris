636ebc6df133dfd2d2de84efb660c5f28fd53219
Ramirisu <labyrinth.ramirisu@gmail.com>
2022-04-29 11:43:34 +0800

    Add partial specializations for range_adaptor_closure.

--- include/iris/ranges/range_adaptor_closure.hpp
+++ include/iris/ranges/range_adaptor_closure.hpp
@@ -5,73 +5,103 @@
 #include <ranges>
 
 namespace iris::ranges {
+namespace __range_adaptor_closure_detail {
+    template <typename T>
+    using __pipeable_base =
+#if defined(_MSC_VER)
+        std::ranges::_Pipe::_Base<T>
+#elif defined(__GNUC__) && __GNUC__ >= 11
+        std::ranges::views::__adaptor::_Partial<T>
+#endif
+        ;
+}
 
 template <typename Fn, typename... Args>
 class range_adaptor_closure
-#if defined(_MSC_VER)
-    : public std::ranges::_Pipe::_Base<range_adaptor_closure<Fn, Args...>>
-#elif defined(__GNUC__) && __GNUC__ >= 11
-    : public std::ranges::views::__adaptor::_Partial<
-          range_adaptor_closure<Fn, Args...>>
-#endif
-{
+    : public __range_adaptor_closure_detail::__pipeable_base<
+          range_adaptor_closure<Fn, Args...>> {
 public:
-    range_adaptor_closure() requires(sizeof...(Args) == 0) = default;
-
     template <typename... Args2>
-        requires(sizeof...(Args) > 0)
     constexpr explicit range_adaptor_closure(Args2&&... args)
         : args_(std::forward<Args2>(args)...)
     {
     }
 
-    void operator()(auto&&) & = delete;
-    void operator()(auto&&) const& = delete;
-    void operator()(auto&&) && = delete;
-    void operator()(auto&&) const&& = delete;
-
-    template <typename T>
-        requires std::invocable<Fn, T, Args...>
-    constexpr decltype(auto) operator()(T&& t) &
+    template <typename Range>
+        requires std::invocable<Fn, Range, const Args&...>
+    constexpr auto operator()(Range&& range) const&
     {
-        return invoke(*this, std::forward<T>(t),
-                      std::index_sequence_for<Args...> {});
+        return std::apply(
+            [&](const auto&... args) {
+                return Fn {}(std::forward<Range>(range), args...);
+            },
+            args_);
     }
 
-    template <typename T>
-        requires std::invocable<Fn, T, const Args...>
-    constexpr decltype(auto) operator()(T&& t) const&
+    template <typename Range>
+        requires std::invocable<Fn, Range, Args...>
+    constexpr auto operator()(Range&& range) &&
     {
-        return invoke(*this, std::forward<T>(t),
-                      std::index_sequence_for<Args...> {});
-    }
-
-    template <typename T>
-        requires std::invocable<Fn, T, Args...>
-    constexpr decltype(auto) operator()(T&& t) &&
-    {
-        return invoke(*this, std::forward<T>(t),
-                      std::index_sequence_for<Args...> {});
-    }
-
-    template <typename T>
-        requires std::invocable<Fn, T, const Args...>
-    constexpr decltype(auto) operator()(T&& t) const&&
-    {
-        return invoke(*this, std::forward<T>(t),
-                      std::index_sequence_for<Args...> {});
+        return std::apply(
+            [&](auto&... args) {
+                return Fn {}(std::forward<Range>(range), std::move(args)...);
+            },
+            args_);
     }
 
 private:
-    template <typename Self, typename T, std::size_t... Indexes>
-    static constexpr decltype(auto)
-    invoke(Self&& self, T&& t, std::index_sequence<Indexes...>)
+    std::tuple<Args...> args_;
+};
+
+template <typename Fn, typename Arg>
+class range_adaptor_closure<Fn, Arg>
+    : public __range_adaptor_closure_detail::__pipeable_base<
+          range_adaptor_closure<Fn, Arg>> {
+public:
+    template <typename Arg2>
+    constexpr explicit range_adaptor_closure(Arg2&& arg)
+        : arg_(std::forward<Arg2>(arg))
     {
-        return Fn {}(std::forward<T>(t),
-                     std::get<Indexes>(std::forward<Self>(self).args_)...);
     }
 
-    std::tuple<Args...> args_;
+    template <typename Range>
+        requires std::invocable<Fn, Range, const Arg&>
+    constexpr auto operator()(Range&& range) const&
+    {
+        return Fn {}(std::forward<Range>(range), arg_);
+    }
+
+    template <typename Range>
+        requires std::invocable<Fn, Range, Arg>
+    constexpr auto operator()(Range&& range) &&
+    {
+        return Fn {}(std::forward<Range>(range), std::move(arg_));
+    }
+
+private:
+    Arg arg_;
+};
+
+template <typename Fn>
+class range_adaptor_closure<Fn>
+    : public __range_adaptor_closure_detail::__pipeable_base<
+          range_adaptor_closure<Fn>> {
+public:
+    range_adaptor_closure() = default;
+
+    template <typename Range>
+        requires std::invocable<Fn, Range>
+    constexpr auto operator()(Range&& r) const&
+    {
+        return Fn {}(std::forward<Range>(r));
+    }
+
+    template <typename Range>
+        requires std::invocable<Fn, Range>
+    constexpr auto operator()(Range&& r) &&
+    {
+        return Fn {}(std::forward<Range>(r));
+    }
 };
 
 }
