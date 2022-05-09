1ed73f094093c5b2fda7cbce2173a86b8f2f193d
Ramirisu <labyrinth.ramirisu@gmail.com>
2022-05-09 23:27:07 +0800

    Add common_type partial specialization for pair and tuple.

--- include/iris/compat.hpp
+++ include/iris/compat.hpp
@@ -1,8 +1,6 @@
 #pragma once
 
 #if defined(__cpp_lib_ranges_zip) && __cpp_lib_ranges_zip >= 202110L
-// already implemented basic_common_reference partial specialization for pair
-// and tuple.
 #else
 
 #include <tuple>
@@ -24,12 +22,18 @@ template <class T1,
         typename pair<common_reference_t<TQual<T1>, UQual<U1>>,
                       common_reference_t<TQual<T2>, UQual<U2>>>;
     }
-struct basic_common_reference<std::pair<T1, T2>,
-                              std::pair<U1, U2>,
-                              TQual,
-                              UQual> {
+struct basic_common_reference<pair<T1, T2>, pair<U1, U2>, TQual, UQual> {
     using type = pair<common_reference_t<TQual<T1>, UQual<U1>>,
                       common_reference_t<TQual<T2>, UQual<U2>>>;
+};
+
+template <class T1, class T2, class U1, class U2>
+    requires requires
+    {
+        typename pair<common_type_t<T1, U1>, common_type_t<T2, U2>>;
+    }
+struct common_type<pair<T1, T2>, pair<U1, U2>> {
+    using type = pair<common_type_t<T1, U1>, common_type_t<T2, U2>>;
 };
 
 template <class... Ts,
@@ -40,10 +44,19 @@ template <class... Ts,
           class UQual>
     requires requires
     {
-        typename tuple<std::common_reference_t<TQual<Ts>, UQual<Us>>...>;
+        typename tuple<common_reference_t<TQual<Ts>, UQual<Us>>...>;
     }
 struct basic_common_reference<tuple<Ts...>, tuple<Us...>, TQual, UQual> {
     using type = tuple<common_reference_t<TQual<Ts>, UQual<Us>>...>;
+};
+
+template <class... Ts, class... Us>
+    requires requires
+    {
+        typename tuple<common_type_t<Ts, Us>...>;
+    }
+struct common_type<tuple<Ts...>, tuple<Us...>> {
+    using type = tuple<common_type_t<Ts, Us>...>;
 };
 
 }
